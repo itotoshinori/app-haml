6a1e7f368190d6f72228188fab231215c5906e88
T.Ito <tito40358@gmail.com>
2022-08-09 18:39:11 +0000

    バッチファイル変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -52,7 +52,7 @@ class TweetsController < ApplicationController
     @tweet.destroy
 
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
+      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed" }
       format.json { head :no_content }
     end
   end
